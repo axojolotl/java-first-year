600aae5f2a9d5359d2016ac7992dd71cff0e8497
wafforyu <wafforyu@gmail.com>
2021-05-01 16:26:23 +0800

    update MainProgram.main method

--- intermediate_prog/src/activity_0/main_package/MainProgram.java
+++ intermediate_prog/src/activity_0/main_package/MainProgram.java
@@ -7,90 +7,23 @@ import java.util.Scanner;
 
 public class MainProgram {
     public static void main(String[] args) {
-
         Scanner scDouble = new Scanner(System.in);
         Scanner scInt = new Scanner(System.in);
         Scanner scString = new Scanner(System.in);
 
-        //STUDENT INFO
-        System.out.print("STUDENT DETAILS\nStudent Name: ");
-        String name = scString.nextLine();
-        System.out.print("Age: ");
-        int age = scInt.nextInt();
-        System.out.print("Student Gender: ");
-        String gender = scString.nextLine();
-        System.out.print("Student Address: ");
-        String address = scString.nextLine();
-        System.out.print("Contact Number: ");
-        int contactNumber = scInt.nextInt();
-        System.out.print("Mother's Name: ");
-        String motherName = scString.nextLine();
-        System.out.print("Father's Name: ");
-        String fatherName = scString.nextLine();
+        StudentInfo s = new StudentInfo(scString.nextLine(),
+                scInt.nextByte(), scString.nextLine(),
+                scString.nextLine(), scInt.nextInt(),
+                scString.nextLine(),scString.nextLine());
+        AcadInfo a = new AcadInfo(scDouble.nextDouble(), scDouble.nextDouble(), scDouble.nextDouble(), scDouble.nextDouble());
+        EmployeeInfo e = new EmployeeInfo(scString.nextLine(), scString.nextLine(), scString.nextLine());
 
-        StudentInfo s = new StudentInfo(name, age, gender, address, contactNumber, motherName, fatherName);
-
-        //EMPLOYEE DETAILS
-        System.out.print("Employee Name: ");
-        String employeeName = scString.nextLine();
-        System.out.print("Designation: ");
-        String designation = scString.nextLine();
-        System.out.print("Academic Position: ");
-        String position = scString.nextLine();
-        EmployeeInfo e = new EmployeeInfo(employeeName,designation,position);
-
-        //STUDENT EDUCATIONAL DETAILS INFO
-        System.out.print("quiz1: ");
-        double q1 = scDouble .nextDouble();
-        System.out.print("quiz2: ");
-        double q2 = scDouble.nextDouble();
-        System.out.print("midterm: ");
-        double midterm = scDouble.nextDouble();
-        System.out.print("finals: ");
-        double finals = scDouble.nextDouble();
-        System.out.println();
-
-        AcadInfo a = new AcadInfo(q1, q2, midterm, finals);
-
-        //SECOND STUDENT!!==================================================
-        System.out.print("STUDENT DETAILS\nStudent Name: ");
-        String name1 = scString.nextLine();
-        System.out.print("Age: ");
-        int age1 = scInt.nextInt();
-        System.out.print("Student Gender: ");
-        String gender1 = scString.nextLine();
-        System.out.print("Student Address: ");
-        String address1 = scString.nextLine();
-        System.out.print("Contact Number: ");
-        int contactNumber1 = scInt.nextInt();
-        System.out.print("Mother's Name: ");
-        String motherName1 = scString.nextLine();
-        System.out.print("Father's Name: ");
-        String fatherName1 = scString.nextLine();
-
-        StudentInfo s1 = new StudentInfo(name, age, gender, address, contactNumber, motherName, fatherName);
-
-        //EMPLOYEE DETAILS
-        System.out.print("EMPLOYEE DETAILS\nEmployee Name: ");
-        String employeeName1 = scString.nextLine();
-        System.out.print("Designation: ");
-        String designation1 = scString.nextLine();
-        System.out.print("Academic Position: ");
-        String position1 = scString.nextLine();
-        EmployeeInfo e1 = new EmployeeInfo(employeeName,designation,position);
-
-        //STUDENT EDUCATIONAL DETAILS INFO
-        System.out.print("STUDENT EDUC DETAILS\nquiz1: ");
-        double q11 = scDouble .nextDouble();
-        System.out.print("quiz2: ");
-        double q22 = scDouble.nextDouble();
-        System.out.print("midterm: ");
-        double midterm1 = scDouble.nextDouble();
-        System.out.print("finals: ");
-        double finals1 = scDouble.nextDouble();
-        System.out.println();
-
-        AcadInfo a1 = new AcadInfo(q11, q22, midterm1, finals1);
+        StudentInfo s1 = new StudentInfo(scString.nextLine(),
+                scInt.nextByte(), scString.nextLine(),
+                scString.nextLine(), scInt.nextInt(),
+                scString.nextLine(),scString.nextLine());
+        AcadInfo a1 = new AcadInfo(scDouble.nextDouble(), scDouble.nextDouble(), scDouble.nextDouble(), scDouble.nextDouble());
+        EmployeeInfo e1 = new EmployeeInfo(scString.nextLine(), scString.nextLine(), scString.nextLine());
 
         s.displayStudent();
         e.displayEmployee();
@@ -101,5 +34,7 @@ public class MainProgram {
         s1.displayStudent();
         e1.displayEmployee();
         a1.displayEducDetails();
+
+
     }
 }
