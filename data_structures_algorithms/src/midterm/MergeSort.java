189cda2907e869957600b2b6ae253bdfbb49ed28
wafforyu <wafforyu@gmail.com>
2021-05-17 22:22:17 +0800

    fix merge sort for midterm

--- data_structures_algorithms/src/midterm/MergeSort.java
+++ data_structures_algorithms/src/midterm/MergeSort.java
@@ -2,17 +2,18 @@ package midterm;
 
 import java.util.Arrays;
 
-public class MergeSort{
-    private static void merge(int[] arr, int low, int mid, int high){
+public class MergeSort {
+
+    private static void merge(int[] arr, int low, int mid, int high) {
         int[] leftArray = new int[mid-low+1];
         int[] rightArray = new int[high-mid];
 
         for (int i = 0; i < leftArray.length ; i++) { leftArray[i] = arr[low+i]; }
         for (int j = 0; j < rightArray.length; j++) { rightArray[j] = arr[mid+j+1]; }
 
-        int i = 0, j = 0, k = low;
+        int i=0, j=0, k=low;
 
-        while(i < leftArray.length && j < rightArray.length){
+        while (i < leftArray.length && j < rightArray.length){
             if (leftArray[i] <= rightArray[j]){
                 arr[k] = leftArray[i];
                 i++;
@@ -38,12 +39,21 @@ public class MergeSort{
 
     private static void mergeSort(int[] arr, int low, int high){
         if (low < high){
-            int mid = (low+high)/2;
+            int mid = (low + high)/2;
 
-            mergeSort(arr,low,mid);
-            mergeSort(arr,mid+1,high);
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid+1, high);
 
             merge(arr,low,mid,high);
+
         }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {6,4,2,1,45,2,3,6,7,8,9,5,345,53,42,42,12,453,89,0};
+
+        System.out.println(Arrays.toString(nums));
+        mergeSort(nums, 0, nums.length-1);
+        System.out.println(Arrays.toString(nums));
     }
 }
