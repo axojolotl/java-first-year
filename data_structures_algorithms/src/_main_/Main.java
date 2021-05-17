f19477421b52ea1faa8a733d55c1cf54970d67ab
wafforyu <wafforyu@gmail.com>
2021-05-17 21:34:10 +0800

    merge sort for midterm

--- data_structures_algorithms/src/_main_/Main.java
+++ data_structures_algorithms/src/_main_/Main.java
@@ -1,9 +1,6 @@
 package _main_;
-import activity1.*;
 
 public class Main {
     public static void main(String[] args) {
-        loopingFib.run();
-        sequentialFib.run();
-    }    
+    }
 }
---
+++ data_structures_algorithms/src/midterm/MergeSort.java
@@ -0,0 +1,49 @@
+package midterm;
+
+import java.util.Arrays;
+
+public class MergeSort{
+    private static void merge(int[] arr, int low, int mid, int high){
+        int[] leftArray = new int[mid-low+1];
+        int[] rightArray = new int[high-mid];
+
+        for (int i = 0; i < leftArray.length ; i++) { leftArray[i] = arr[low+i]; }
+        for (int j = 0; j < rightArray.length; j++) { rightArray[j] = arr[mid+j+1]; }
+
+        int i = 0, j = 0, k = low;
+
+        while(i < leftArray.length && j < rightArray.length){
+            if (leftArray[i] <= rightArray[j]){
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftArray.length){
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightArray.length){
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static void mergeSort(int[] arr, int low, int high){
+        if (low < high){
+            int mid = (low+high)/2;
+
+            mergeSort(arr,low,mid);
+            mergeSort(arr,mid+1,high);
+
+            merge(arr,low,mid,high);
+        }
+    }
+}
